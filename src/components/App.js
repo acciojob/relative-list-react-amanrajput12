016d63cfccf16d030bec81131c58b4cf93ac3842
Aman Rajput <rajputaman724857@gmail.com>
2025-05-06 13:22:47 +0530

    add key in ol

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const App = () => {
+  const list =[1,3,4,43,43];
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key={"relativelist"}>
+                {list.map((data,i)=><li key={`relativeListItem${i}`}>{data}</li>)}
+               </ol>
     </div>
   )
 }
